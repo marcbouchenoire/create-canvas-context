93cf0cb00f56c3f761548ce6f79d1837973946cc
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2021-12-22 23:42:48 +0100

    Add JSDoc comments

--- src/utils/return-if.ts
+++ src/utils/return-if.ts
@@ -1,5 +1,11 @@
 import { Guard } from "../types"
 
+/**
+ * Return a value only if a given type guard returns positively.
+ *
+ * @param value - The value to be checked.
+ * @param guard - The type guard to check.
+ */
 export function returnIf<T>(value: T, guard: Guard<NonNullable<T>>) {
   return guard(value) ? value : null
 }
--- src/utils/supports-canvas.ts
+++ src/utils/supports-canvas.ts
@@ -1,7 +1,13 @@
+/**
+ * Return whether `HTMLCanvasElement` exists.
+ */
 export function supportsCanvas() {
   return typeof HTMLCanvasElement !== "undefined"
 }
 
+/**
+ * Return whether `OffscreenCanvas` exists.
+ */
 export function supportsOffscreenCanvas() {
   return typeof OffscreenCanvas !== "undefined"
 }
